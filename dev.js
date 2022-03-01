155bfdb6c1a647ec374b45e1fa93abcecb302cbe
mattrossman <rossman@gatech.edu>
2022-02-28 22:32:47 -0500

    Remove THREE boilerplate to debug Hallway

--- dev.js
+++ dev.js
@@ -1,28 +1,32 @@
 require("dotenv").config()
 const { NodeGlobalsPolyfillPlugin } = require("@esbuild-plugins/node-globals-polyfill")
 const { NodeModulesPolyfillPlugin } = require("@esbuild-plugins/node-modules-polyfill")
+const esbuild = require("esbuild")
 
-require("esbuild")
-  .serve(
-    {
-      servedir: "public",
-    },
-    {
-      entryPoints: ["main.js"],
-      format: "esm",
-      bundle: true,
-      outdir: "public/js",
-      define: {
-        AVATAR_WEBKIT_AUTH_TOKEN: `\"${process.env.AVATAR_WEBKIT_AUTH_TOKEN}\"`,
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    }
-  )
+/** @type {esbuild.ServeOptions} */
+const serveOptions = {
+  servedir: "public",
+  port: 3000,
+}
+
+/** @type {esbuild.BuildOptions} */
+const buildOptions = {
+  entryPoints: ["main.js"],
+  bundle: true,
+  outdir: "public/js",
+  define: {
+    AVATAR_WEBKIT_AUTH_TOKEN: `\"${process.env.AVATAR_WEBKIT_AUTH_TOKEN}\"`,
+  },
+  plugins: [
+    NodeGlobalsPolyfillPlugin({
+      process: true,
+    }),
+    NodeModulesPolyfillPlugin(),
+  ],
+}
+
+esbuild
+  .serve(serveOptions, buildOptions)
   .then((server) => {
     console.log(`Server is running on http://${server.host}:${server.port}`)
   })
--- main.js
+++ main.js
@@ -1,67 +1,28 @@
-import * as THREE from "three"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import { AUPredictor } from "@quarkworks-inc/avatar-webkit"
 
-// Scene, Camera
-const scene = new THREE.Scene()
-scene.background = new THREE.Color("black")
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(0, 0, 3)
+async function initHallwaySDK() {
+  const videoStream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      width: { ideal: 640 },
+      height: { ideal: 360 },
+      facingMode: "user",
+    },
+  })
 
-// WebGL renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true })
-renderer.setPixelRatio(2)
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+  console.log(`using auth token ${AVATAR_WEBKIT_AUTH_TOKEN}`)
 
-// OrbitControls
-const controls = new OrbitControls(camera, renderer.domElement)
+  const predictor = new AUPredictor({
+    apiToken: AVATAR_WEBKIT_AUTH_TOKEN,
+    srcVideoStream: videoStream,
+  })
 
-// Mesh
-const geometry = new THREE.IcosahedronGeometry()
-const material = new THREE.MeshNormalMaterial()
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+  predictor.onPredict = (results) => {
+    console.log(results)
+  }
 
-// Render loop
-const clock = new THREE.Clock()
-
-function render() {
-  const dt = clock.getDelta()
-
-  controls.update()
-
-  renderer.render(scene, camera)
-}
-renderer.setAnimationLoop(render)
-
-// Window sizing
-window.addEventListener("resize", onWindowResize)
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  await predictor.start()
+  console.log("Predictor started...")
 }
 
-// Hallway SDK
-let videoStream = await navigator.mediaDevices.getUserMedia({
-  audio: false,
-  video: {
-    width: { ideal: 640 },
-    height: { ideal: 360 },
-    facingMode: "user",
-  },
-})
-
-let predictor = new AUPredictor({
-  apiToken: AVATAR_WEBKIT_AUTH_TOKEN,
-  srcVideoStream: videoStream,
-})
-
-predictor.onPredict = (results) => {
-  window.results = results
-}
-
-await predictor.start()
-console.log("Predictor started...")
+initHallwaySDK()
